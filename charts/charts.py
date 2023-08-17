9a9cefcde95bec7f7af2f6abdf04c90eb7f26be1
Jose Sandoya <joseivansandoya@gmail.com>
2023-08-16 20:01:45 -0500

    create charts

---
+++ charts/charts.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+
+def generate_pie_chart():
+    labels = ['Python', 'C++', 'Ruby', 'Java']
+    sizes = [215, 130, 245, 210]
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels)
+    plt.savefig('generated/pie_chart.png')
+    plt.close()
