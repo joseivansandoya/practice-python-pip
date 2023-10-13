339f87a0a49f77f655abd34ed20bf95ebf67282c
José I. Sandoya <joseivansandoya@gmail.com>
2023-10-12 21:32:45 -0500

    Update charts.py

--- charts/charts.py
+++ charts/charts.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 def generate_pie_chart():
     labels = ['Python', 'C++', 'Ruby', 'Java']
     sizes = [215, 130, 245, 210]
+    print("hi")
 
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels)
